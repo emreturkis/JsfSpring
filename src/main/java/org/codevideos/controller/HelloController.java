fb41b1daeeb9ca879fc568caffdaff87c80b4a15
Emre TÜRKİŞ <emreturkis@gmail.com>
2015-11-24 23:44:41 +0200

    scope changed

--- src/main/java/org/codevideos/controller/HelloController.java
+++ src/main/java/org/codevideos/controller/HelloController.java
@@ -1,21 +1,21 @@
 package org.codevideos.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.codevideos.service.api.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import javax.servlet.ServletContext;
 import java.io.Serializable;
 
 /**
  * Created by eturkis on 11/24/2015.
  */
-@ViewScoped
+@RequestScoped
 @ManagedBean(name = "helloController")
 public class HelloController implements Serializable {
     @Autowired
